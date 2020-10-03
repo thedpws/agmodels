08e5da0c834383ded9a6dae51658f70d92982349
Az Vasquez <azvasquez99@gmail.com>
2020-10-03 12:35:42 -0400

    Add titles to courses

--- models.py
+++ models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Course(models.Model):
     lms_id = models.CharField(max_length=20)
 
+    name = models.CharField(max_length=100, blank=True, null=True)
+
     def __str__(self):
         return f'Course {self.lms_id}'
 
