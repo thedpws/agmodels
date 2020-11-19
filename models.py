7a8abf382716ea656cf1506ec34e061e597bc977
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2020-11-18 22:51:58 -0500

    no hpg

--- models.py
+++ models.py
@@ -222,5 +222,5 @@ class Resource(models.Model):
     testcase_as_input = models.ForeignKey(TestCase, on_delete=models.CASCADE, blank=True, null=True, related_name='input_resources')
     testcase_as_output = models.ForeignKey(TestCase, on_delete=models.CASCADE, blank=True, null=True, related_name='output_resources')
 
-    file = models.FileField(upload_to=resource_filepath_function, storage=HiPerGatorStorage)
+    file = models.FileField(upload_to=resource_filepath_function)
 
